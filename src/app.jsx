78533c9e3ded7a2b60c5f3421462ae8d013d8214
elliottdaviess <elliott_davies@live.co.uk>
2022-05-01 19:51:20 +0100

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.scss";
+
+const App = () => {
+  return <h1>Hello world</h1>;
+};
+
+export default App;
