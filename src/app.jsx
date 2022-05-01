924b93588253bf4430ff9fb73145570a96936f18
elliottdaviess <elliott_davies@live.co.uk>
2022-05-01 20:14:10 +0100

    Task 1 complete
    Retrieving and displaying payments

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,37 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
+import { getAllPayments } from "./services/paymentsService";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.scss";
+import PaymentsTable from "./components/Payments/PaymentsTable";
 
 const App = () => {
-  return <h1>Hello world</h1>;
+  const [paymentsData, setPaymentsData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getPaymentsData = async () => {
+    const response = await getAllPayments();
+    setPaymentsData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getPaymentsData();
+  }, []);
+
+  return (
+    <>
+      {isLoading && <h1>Loading...</h1>}
+      {!isLoading && !paymentsData.results && (
+        <h1>Oops...something went wrong</h1>
+      )}
+      {!isLoading && paymentsData.results && (
+        <div className='container'>
+          <h1>Payment Transactions</h1>
+          <PaymentsTable transactions={paymentsData.results}></PaymentsTable>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default App;
