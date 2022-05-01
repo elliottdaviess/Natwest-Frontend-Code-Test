195eb3c966c4256137b339bf361835ce518c7df7
elliottdaviess <elliott_davies@live.co.uk>
2022-05-01 20:13:21 +0100

    Correcting stub typo

--- stub-api/generate-payments.js
+++ stub-api/generate-payments.js
@@ -11,7 +11,7 @@ const generatePayment = (date) => ({
   paymentType: randomBool() ? "CHAPS" : "FASTER",
   paymentDate: date.format("DD-MMM-YYYY"),
   paymentStatus: randomBool(3) ? "C" : randomBool(2) ? "P" : "A",
-  toAccaunt: {
+  toAccount: {
     accountName: faker.finance.accountName(),
     sortCode: randomString.generate({ length: 6, charset: "numeric" }),
     accountNumber: randomString.generate({ length: 8, charset: "numeric" }),
